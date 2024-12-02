5fb9b614e00d9139bb152d0dccf4777f5d3998bc
Yunus Habeeb <habeebdh1@gmail.com>
2024-12-02 12:12:02 +0100

    Implement readability by via more components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,45 +1,9 @@
-import Tilt from 'react-parallax-tilt';
 import { motion } from 'framer-motion';
-
 import { styles } from '../styles';
 import { services } from '../constants';
 import { SectionWrapper } from '../hoc';
 import { fadeIn, textVariant } from '../utils/motions';
-
-const ServiceCard = ({
-  index,
-  title,
-  icon,
-}: {
-  index: number;
-  title: string;
-  icon: string;
-}) => (
-  <Tilt
-    tiltMaxAngleX={45}
-    tiltMaxAngleY={45}
-    scale={1}
-    transitionSpeed={450}
-    className="xs:w-[250px] w-full"
-  >
-    <motion.div
-      variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
-      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-    >
-      <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
-        <img
-          src={icon}
-          alt="web-development"
-          className="w-16 h-16 object-contain"
-        />
-
-        <h3 className="text-white text-[20px] font-bold text-center">
-          {title}
-        </h3>
-      </div>
-    </motion.div>
-  </Tilt>
-);
+import ServiceCard from './ServiceCard';
 
 const About = () => {
   return (
---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+declare interface ExperienceProps {
+  title: string;
+  company_name: string;
+  icon: string;
+  iconBg: string;
+  date: string;
+  points: string[];
+}
